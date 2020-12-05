9f417f520995bf7016246d31e09b057a36e8e22b
Zbigniew "Radagast" Kaleta <zkaleta@agh.edu.pl>
2020-12-05 19:30:39 +0100

    Komentarz do lab 6

--- zad6/line_equation.py
+++ zad6/line_equation.py
@@ -1,12 +1,13 @@
 def line_equation(pointA, pointB):
     result = ()
     sub = (pointA[0] - pointB[0])
-    if pointA[0] == pointB[0] and pointA[1] == pointB[1]:
-        raise ValueError('The points cannot be the same')
+    if pointA[0] == pointB[0] and pointA[1] == pointB[1]:   # pointA == pointB
+        raise ValueError('The points cannot be the same')   # czemu?
     if sub != 0:
         coordinate1 = (pointA[1] - pointB[1]) / sub
         coordinate2 = pointA[1] - ((pointA[1] - pointB[1]) / sub) * pointA[0]
         result = (coordinate1, coordinate2)
+        # else?
     return result
 
 
--- zad6/quadratic_equation.py
+++ zad6/quadratic_equation.py
@@ -3,24 +3,24 @@ def square_root(x):
 
 
 def solve(a, b, c):
-    a = float(a)
+    a = float(a)    # można założyć poprawny typ
     b = float(b)
     c = float(c)
 
     if a == b == c == 0:
-        raise ValueError("All coefficients can't be zero")
+        raise ValueError("All coefficients can't be zero")  # czemu?
     if type(a) != float or type(b) != float or type(c) != float:
         raise ValueError('Coefficients should be float')
     if a == 0 and b != 0:
         return -c / b
     if a == b == 0 and c != 0:
-        raise ValueError("The equation is not a quadratic equation")
+        raise ValueError("The equation is not a quadratic equation") # to we wcześniejszym if'ie też nie
 
-    result = ()
+    result = () # ta wartość nie jest używana
     delta = b ** 2 - 4 * a * c
     if delta < 0:
         return ()
-    x1 = (-b - square_root(delta)) / 2 * a
+    x1 = (-b - square_root(delta)) / 2 * a  # kolejność wykonywania działań
     x2 = (-b + square_root(delta)) / 2 * a
     x0 = -b / 2 * a
     if delta > 0:
@@ -29,3 +29,4 @@ def solve(a, b, c):
         result = x0
 
     return result
+    # raz liczba, raz krotka
