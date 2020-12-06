8e967932fa985a1224af22da944aefd75681446f
karolinamaruszak <karolina.maruszak@onet.eu>
2020-12-06 22:52:42 +0100

    zad7

---
+++ zad7/loading_training_data.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+class LoadCSV:
+
+    def __init__(self, path):
+        with open(path, encoding='utf-8') as csv_file:
+            self.csv_data = np.genfromtxt(csv_file, delimiter=';')
+
+    def get_label(self):
+        return self.csv_data[:, ~0]
+
+    def column_normalization(self, col):
+        ''' Nie do konca jestem pewna czy chodzilo o zwrocenie jednej kolumny czy o podmiane kolumny w calej tablicy'''
+        col -= 1
+        if 0 < col < self.csv_data.shape[1] - 1 and isinstance(col, int):
+            self.csv_data[:, col] = self.csv_data[:, col] / np.linalg.norm(self.csv_data[:, col])  # podmieniam kolumne
+        else:
+            raise ValueError(f"Column must be integer in range (1, {self.csv_data.shape[1] - 1})")
+
+    def column_centering(self, col):
+        ''' Nie do konca jestem pewna czy chodzilo o zwrocenie jednej kolumny czy o podmiane kolumny w calej tablicy'''
+        col -= 1
+        if 0 < col < self.csv_data.shape[1] - 1 and isinstance(col, int):
+            mean_value = self.csv_data[:, col] - self.csv_data[:, col].mean()
+            std_value = self.csv_data[:, col].std()
+            self.csv_data[:, col] = np.divide(mean_value, std_value, where=std_value != 0)  # podmieniam kolumne
+        else:
+            raise ValueError(f"Column must be integer in range (1, {self.csv_data.shape[1] - 1})")
+
+    def rows_normalization(self):
+        ''' Nie do konca jestem pewna czy chodzilo o zwrocenie znormalizowanej tablicy czy o jej podmiane '''
+        self.csv_data = self.csv_data / np.linalg.norm(self.csv_data, axis=0)
+
+    def getX(self):
+        return self.csv_data[:, :self.csv_data.shape[1] - 1]
+
+
+l = LoadCSV('sample1.csv')
+# print(l.column_normalization(2))
+# print(l.column_centering(2))
+# print(l.get_label())
+# print(l.rows_normalization())
+# print(l.getX())
---
+++ zad7/perceptron.py
@@ -0,0 +1,44 @@
+from jezyki_biblioteki_analizy_danych.zad7.loading_training_data import LoadCSV
+import numpy as np
+
+
+class Perceptron:
+    def __init__(self, data):
+        self.data = data
+        self.x, self.y = data.getX(), data.get_label()
+        self.y -= 0.5
+        self.y = self.y * 2
+        self.x_train, self.x_test = data.getX()[:int(data.getX().shape[0] * 0.6)], \
+                                    data.getX()[int(data.getX().shape[0] * 0.6):]
+        self.y_train, self.y_test = data.get_label()[:int(data.get_label().shape[0] * 0.6)], \
+                                    data.get_label()[int(data.get_label().shape[0] * 0.6):]
+        self.y_test += 0.5
+        self.weights = np.zeros(data.getX().shape[1])
+        self.bias = 0
+
+    def train(self, max_iter=62):
+        for epoch in range(max_iter):
+            error = 0
+            loss = 0
+            for i in range(self.x_train.shape[0]):
+                activation = self.weights @ self.x_train[i]
+                if activation * self.y_train[i] <= 0:
+                    self.weights += self.y_train[i] * self.x_train[i]
+                    self.bias += self.y_train[i]
+                    error += 1
+                loss += max(0, 1 - activation * self.y_train[i])
+            print(
+                f"epoch={epoch + 1}, loss={loss / self.x_train.shape[0]}, accuracy={1 - error / self.x_train.shape[0]}")
+
+    def predict(self, X, Y):
+        print(f"accuracy = {sum(list(map(lambda x: int(x), self.weights @ X.T + self.bias > 0)) == Y)/Y.shape[0]}")
+        return list(map(lambda x: int(x), self.weights @ X.T + self.bias > 0)) == Y
+
+
+l = LoadCSV('sample1.csv')
+p = Perceptron(l)
+p.train()
+# p.predict(p.x_test)
+# print(p.x_test)
+p.predict(np.array([[7,21,21,-9,56,-8,17,44]]), np.array([0]))
+p.predict(p.x_test, p.y_test)
